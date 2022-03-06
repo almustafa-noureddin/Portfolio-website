7fad7afd591b7dd9b495f3ce86087953f57ab408
almustafa-noureddin <58104742+almustafa-noureddin@users.noreply.github.com>
2022-03-06 13:03:32 +0200

    fixing deployment issues

--- almustafa_website/settings.py
+++ almustafa_website/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         'NAME': env("DB_NAME"),
         'USER': env("DB_USER"),
         'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DATABASE_URL"),
+        'HOST': env("DB_HOST"),
         'PORT': env("DB_PORT"),
     }
 }
